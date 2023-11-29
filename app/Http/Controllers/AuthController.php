4d52f9b49a426f19cfda5b0224432a94562be178
Muhamad Zidane Arfani <30400115+zicchi@users.noreply.github.com>
2023-11-29 23:40:18 +0700

    Update AuthController.php
    WIP

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -3,8 +3,26 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class AuthController extends Controller
 {
-    //
+    public function index(){
+        return view('pages.auth.login');
+    }
+
+    public function login(Request $request){
+        $username = $request->input('username');
+        $password = $request->input('password');
+
+        $auth = Auth::attempt([
+            'username' => $username,
+            'password' => $password,
+        ]);
+
+        if ($auth) {
+            # code...
+        }
+
+    }
 }
